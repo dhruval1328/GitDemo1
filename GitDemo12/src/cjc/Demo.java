0cf20095b49720628a8337f228b193eb60869d75
dhruval1328 <sonidhruval45@gmail.com>
2024-08-02 15:48:06 +0530

    comit

--- GitDemo12/src/cjc/Demo.java
+++ GitDemo12/src/cjc/Demo.java
@@ -4,6 +4,9 @@ public class Demo {
 	public void m1() {
 		System.out.println("method m1");
 	}
+	public void m() {
+		System.out.println("method m1");
+	}
 	public static void main(String[] args) {
 		System.out.println("Main method..");
 	}
